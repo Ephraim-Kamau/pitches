f14598473345ba7e9596121445743ca8cada3337
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-22 17:15:42 +0300

    Add the User and Role models

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     pass
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephraim:123456@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephraim:123@localhost/pitches'
 
 
 
