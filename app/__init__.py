d7fab1af031efddbcedd89b0097cc832562e41e4
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-22 18:05:54 +0300

    Add password authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -22,5 +22,8 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
      # setting config
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate') 
    
     return app
