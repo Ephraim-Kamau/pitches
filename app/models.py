acaf80c1e7c3daf151b8067cc6677f282d54d2f3
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-22 17:01:45 +0300

    Add flask-script shell

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Pitch:
     '''
     Pitch class to define Pitch objects
@@ -33,3 +35,11 @@ class Reviews:
                 response.append(review)
 
         return response    
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'        
