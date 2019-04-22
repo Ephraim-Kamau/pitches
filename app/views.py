a4235f57548e8b57eecb0aadc505e8eac45c12d6
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-22 11:51:04 +0300

    Add the pitch details in views

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app
 from .request  import get_pitches
 
 
+
 # Views
 @app.route('/')
 def index():
@@ -13,3 +14,35 @@ def index():
     
     title = 'Home - Welcome to the best Pitches Site'
     return render_template('index.html', title = title)
+
+app.route('/pickUpLines/')
+def pickUpLines():
+    '''
+    View categories page function that returns the pickUpLines category and its data
+    '''
+    pitches = Pitch.get_pitches('pickUpLines')
+    return render_template('pickUpLines.html', pitches = pitches)    
+
+app.route('/product/')
+def product():
+    '''
+    View categories page function that returns the product pitch category and its data
+    '''
+    pitches = Pitch.get_pitches('product')
+    return render_template('product.html', pitches = pitches)        
+
+app.route('/interview/')
+def interview():
+    '''
+    View categories page function that returns the interview pitch category and its data
+    '''
+    pitches = Pitch.get_pitches('interview')
+    return render_template('interview.html', pitches = pitches)     
+
+app.route('/promotion/')
+def promotion():
+    '''
+    View categories page function that returns the promotion pitch category and its data
+    '''
+    pitches = Pitch.get_pitches('promotion')
+    return render_template('promotion.html', pitches = pitches)
