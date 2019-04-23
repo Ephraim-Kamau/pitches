a7104a646003fd96e6656e84393f063da0792d5f
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-23 09:46:38 +0300

    Add the Log In

--- app/__init__.py
+++ app/__init__.py
@@ -26,8 +26,6 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-     # setting config
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate') 
    
