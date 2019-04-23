72fda38d353204ae492e086c9906d347740641d3
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-23 11:34:32 +0300

    Add the register.html details

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,7 @@ from .. import db
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
